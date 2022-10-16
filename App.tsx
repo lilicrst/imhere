2665fb362439e03c0c12aa71dd2efdd7c681b9f7
lilicrst <lisandra.crisilva37@gmail.com>
2022-10-16 15:56:11 -0300

    first layout

--- App.tsx
+++ App.tsx
@@ -1,20 +1,27 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { Text, View } from 'react-native';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
-}
+    <View style={{
+      flex: 1,
+      backgroundColor: '#131016',
+      padding: 16
+    }}>
+      <Text style={{
+        color: '#FDFCFE',
+        fontSize: 24,
+        fontWeight: 'bold',
+        marginTop: 48
+      }}>
+        React Native
+      </Text>
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+      <Text style={{
+        color: '#6b6b6b',
+        fontSize: 16
+      }}>
+        Projeto iniciado em 16 de Outubro.
+        </Text>
+    </View>
+  )
+}
